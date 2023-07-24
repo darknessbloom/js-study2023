9be6d61d2e0b4bf9fe44497edab41bb8d596ebcf
seon <darkness.blooms.sun@gmail.com>
2023-07-24 15:27:59 +0900

    변수와 상수, 데이터타입, 산술연산자학습

---
+++ 1.core-js/1.basic/3.산술연산자.js
@@ -0,0 +1,27 @@
+//거듭제곱연산 
+let num=3;
+console.log(num**4);
+// 증감연산자
+let x=5;
+console.log(`x:${x++}`);
+console.log(`x:${x}`);
+
+let y=33;
+
+let a=7
+let b=a++;
+console.log(`a:${a} b:${b}`);
+let c=7;
+let d=++c;
+console.log(`c:${c} d:${d}`);
+// 대입연산자
+let z=9;
+z+=1;
+z-=8;
+z**=2;
+console.log(`z:${z}`);
+let m=8;
+ m++;//1증가감소일때는 ++,--가 빠름.
+m+=1;//m=m+1 =,+두번연산함. 
+console.log(`m:${m}`);
+
