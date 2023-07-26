420bdedcb9a041648f90daf9484df5a9df56aed4
seon <darkness.blooms.sun@gmail.com>
2023-07-26 09:32:00 +0900

    1.조건문

---
+++ 1.core-js/2.제어문/1.조건문.js
@@ -0,0 +1,19 @@
+let age=5;
+if(age>=19){
+    console.log('성인');
+}else if(age>=17){
+
+    console.log('고등학생');
+}
+else if(age>=14){
+
+    console.log('중학생');
+}
+else if(age>=8){
+
+    console.log('초등학생');
+}
+else{
+    console.log('미취학아동');
+    
+}
