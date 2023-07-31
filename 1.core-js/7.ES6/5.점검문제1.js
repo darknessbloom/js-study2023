e9e3f06136906da6b8c45bd2f71caf4771f6c136
seon <darkness.blooms.sun@gmail.com>
2023-07-31 09:33:55 +0900

    점검문제 해설

--- 1.core-js/7.ES6/5.점검문제1.js
+++ 1.core-js/7.ES6/5.점검문제1.js
@@ -57,6 +57,8 @@ const traders = [
 
 // 연습 1: 2022년에 발생한 모든 거래를 찾아
 //   거래자 정보(이름, 도시)를 배열에 매핑해주세요
+
+//내꺼
 const trderInfoAt2022 = traders
   .filter((trade) => trade.year === 2022)
   .map((t) => {
@@ -67,9 +69,21 @@ const trderInfoAt2022 = traders
   });
 console.log(trderInfoAt2022);
 
+//해설
+const tradeIn2022=traders.filter(trs => trs.year===2022);
+const traderIn2022=tradeIn2022.map(trs=> trs.trader)
+console.log(tradeIn2022);
+console.log(traderIn2022);
+
 // 연습 2: 거래자가 근무하는 모든 도시이름을 배열에 담아주세요.
 const citys = traders.map((t) => t.trader.city);
 console.log(citys);
+
+//해설
+const mappedCities = traders.map((trs) => trs.trader.city);
+//중복제거
+const cities=[...new Set(mappedCities)];
+console.log(`cities ${cities}`);
 
 // 연습 3: 대전에 근무하는
 // 모든 거래자를 찾아 거래자정보를 배열에 매핑해주세요.
@@ -83,11 +97,20 @@ const workedAtDaejon=traders
                         };
                     });
 console.log(workedAtDaejon);
-
+//해설
+traders
+  .filter(trs=>trs.trader.city==='대전')
+  .map(trs=>trs.trader)
+  .forEach(trd=>console.log(trd));
+          
 
 // 연습 4: 모든 거래자의 이름을 배열에 모아주세요.
 const allTraders = traders.map((t) => t.trader.name);
 console.log(allTraders);
+
+//해설
+const mappedNames=traders.map((t) => t.trader.name);
+console.log(mappedNames);
 
 // 연습 5: 서울에 사는 거래자의 모든 거래액의 총합 출력.
 const totalCostAtSeoul = traders
@@ -98,3 +121,16 @@ totalCostAtSeoul.forEach((v) => {
   totalValue += v;
 });
 console.log(`서울에사는 거래자의 모든 거래액 총합 : ${totalValue} `);
+
+//해설
+let total=0;
+// const filteredTraders=traders
+//   .filter(trs=> trs.trader.city==='서울')
+//   .map(trs=>trs.value)
+//   .forEach(val=>total+=val);
+//reduce 함수:배열을 반복해서 각요소에 콜백함수를 적용한
+//            결과를 누적하는 함수
+const totalvales =traders
+  .filter(trs=> trs.trader.city==='서울')
+  .reduce((acc,curr)=>acc+curr.value,total);
+  console.log(`서울에사는 거래자의 모든 거래액 총합 : ${totalvales}원 `);
