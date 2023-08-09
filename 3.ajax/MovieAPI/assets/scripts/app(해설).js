227e75aae383ad7d6250037f3585ee45e1c5b340
seon <sunkyoung.dev@gmail.com>
2023-08-09 13:32:01 +0900

    ajax-종합실습-일정관리 프로그램- 할일 수정하기

--- 3.ajax/MovieAPI/assets/scripts/app(해설).js
+++ 3.ajax/MovieAPI/assets/scripts/app(해설).js
@@ -2,7 +2,7 @@ const URL = "http://localhost:5000/todos";
 
 const $todoList = document.querySelector(".todo-list");
 
-// 1.db.json에 있는 todo를 화면에 렌더링하기
+// step1. db.json에 있는 todos를 화면에 렌더링하기
 const fetchTodos = (url, method = "GET", payload = null) => {
   const requestInit = {
     method: method,
@@ -12,7 +12,8 @@ const fetchTodos = (url, method = "GET", payload = null) => {
 
   return fetch(url, requestInit);
 };
-//화면에 todos를 랜더링하는 함수
+
+// 화면에 todos를 렌더링하는 함수
 const renderTodos = (todoList) => {
   // li태그의 템플릿을 가져옴
   const $liTemplate = document.getElementById("single-todo");
@@ -23,68 +24,174 @@ const renderTodos = (todoList) => {
     $newLi.querySelector("li").dataset.id = id; // data-id 속성 부여
     $newLi.querySelector(".text").textContent = text;
 
-    //체크박스 가져오기
-    const $checkBox = $newLi.querySelector(".checkbox input[type=checkbox]");
-    $checkBox.checked=done;
-    if(done)$checkBox.parentNode.classList.add('checked');
+    // 체크박스 가져오기
+    const $checkbox = $newLi.querySelector(".checkbox input[type=checkbox]");
+    // console.dir($checkbox);
+    $checkbox.checked = done;
+
+    done && $checkbox.parentNode.classList.add("checked");
+
     $todoList.appendChild($newLi);
   });
 };
-//---------이벤트 관련함수
-const addTodoHandler=e=>{
-    //1. 클릭이벤트가 잘 일어나는가?
-    console.log('클릭');
-    //클릭하면 일단 왼쪽에 인풋의 텍스트를 읽어야함.
-    //2-1 인풋부터 찾자
-    const $textInput=document.getElementById('todo-text');
-    //2-2인풋안에 텟스트를 꺼내자
-    const inputText=$textInput.value;
-    
-    //3. 서버에 이데이터를 보내서 저장해야함
-    //->fetch가 필요 저장이니까 POST!
-    // -> payload를 API 스펙에 맞게 만들어 보내야함.
-    const payload={
-        text:inputText,
-        done:false
+
+// ========= 이벤트 관련 함수 ========= //
+const addTodoHandler = (e) => {
+  // 1. 클릭이벤트가 잘 일어나나?
+  // console.log('클릭!');
+
+  // 2. 클릭하면 일단 왼쪽에 인풋의 텍스트를 읽어야 함.
+  // 2-1. 인풋부터 찾자
+  const $textInput = document.getElementById("todo-text");
+  // 2-2. 인풋 안에 텍스트를 꺼내자
+  const inputText = $textInput.value;
+
+  // 3. 그럼 서버에 이 데이터를 보내서 저장해야 하는데?
+  // -> fetch가 필요하겠다. 저장이니까 POST해야겠다.
+  // -> payload를 API 스펙에 맞게 만들어 보내야 함
+  const payload = {
+    text: inputText,
+    done: false,
+  };
+  fetchTodos(URL, "POST", payload).then((res) => {
+    if (res.status === 200 || res.status === 201) {
+      console.log("등록 성공!");
+    } else {
+      console.log("등록 실패!");
     }
+  });
+};
 
-    fetchTodos(URL,'POST',payload)
-    .then(res=>{
-        if(res.status===200||res.status===201){
-            alert(`등록성공!`);
-        }
-        else{
-            alert(`등록실패!`);
-        }
+// step2. 할 일 등록 기능
+const $addBtn = document.getElementById("add");
+$addBtn.addEventListener("click", addTodoHandler);
+
+// step3. 할 일 삭제 기능
+const deleteTodoHandler = (e) => {
+  if (!e.target.matches(".remove span")) return;
+
+  if (!confirm("진짜로 삭제합니까??")) return;
+
+  // 특정 할일을 지우기 위해 클릭한 할일의 id값을 알아야 함
+  const id = e.target.closest(".todo-list-item").dataset.id;
+  // console.log(id);
+
+  // 서버에 삭제 요청하기
+  fetchTodos(`${URL}/${id}`, "DELETE").then((res) => {
+    if (res.status === 200) {
+      console.log("삭제 성공!");
+    } else {
+      console.log("삭제 실패!");
+    }
+  });
+};
+
+$todoList.addEventListener("click", deleteTodoHandler);
+
+// step4. 할 일 완료 체크 처리
+const checkTodoHandler = (e) => {
+  // console.log('체크박스 누름', e.target);
+
+  // 1. 서버에 수정요청 보내서 누른 그 할일의
+  //    done을 반대값으로 수정해야 함.
+  // 1-1. 현재 체크값인 t, f인지 알아야 반대로바꾸지
+  console.log(e.target.checked); // 현재상태지 이전상태가 아니다
+
+  const id = e.target.closest(".todo-list-item").dataset.id;
+  fetchTodos(`${URL}/${id}`, "PATCH", {
+    done: e.target.checked,
+  });
+};
+
+$todoList.addEventListener("change", checkTodoHandler);
+
+// 수정모드 진입하는 함수
+const enterModifyMode=($undo)=>{
+    //클래스의 이름을 변경하여 아이콘 바꾸기
+    // -> 클릭한 span 태그 노드를 가져와야함
+    $undo.classList.replace('lnr-undo','lnr-checkmark-circle');
+
+    //$undo 근처에있는 span.text가져와야함.
+
+    const $textSpan=$undo.closest('.todo-list-item').querySelector('.text');
+
+    // 교체할 input 생성
+    const $modInput=document.createElement('input');
+    $modInput.classList.add('modify-input')
+    $modInput.setAttribute('type','text');
+    $modInput.value=$textSpan.textContent;
+    //span을 인풋으로
+    const $label=$textSpan.parentNode;
+    $label.replaceChild($modInput,$textSpan);
+
+
+
+};
+const modifyTodo=($checkmark)=>{
+    const $li=$checkmark.closest('.todo-list-item');
+    const id=$li.dataset.id;
+    const newText=$li.querySelector('.modify-input').value;
+    fetchTodos(`${URL}/${id}`,'PATCH',{
+        text:newText
     });
+
 }
 
-//step2 할일 등록기능
-const $addBtn=document.getElementById('add');
-$addBtn.addEventListener('click',addTodoHandler);
+//수정이벤트 처리 핸들러
+const modifyTodoHandler=e=>{
+    if (e.target.matches('.modify span.lnr-undo')){
 
-//step3. 할일 삭제기능
-const deleteTodoHandler=e=>{
-    if(!e.target.matches('.remove span')) return;
-
-    //특정 할일을 지우기위해 그 할일의 아이디 값을 알아야함.
-    const id=e.target.closest('.todo-list-item').dataset.id;
-    // console.log(id);
-    // 서버에 삭제 요청하기
-    fetchTodos(`${URL}/${id}`,'DELETE')
-    .then(res=>{
-        if(res.status===200||res.status===201){
-            alert(`삭제성공!`);
-        }
-        else{
-            alert(`삭제실패!`);
-        }
-    });
+        enterModifyMode(e.target);
+    }
+    else if(e.target.matches('.modify span.lnr-checkmark-circle')){
+        modifyTodo(e.target); //서버에 수정요청보내기
+    }
 }
-$todoList.addEventListener('click',deleteTodoHandler);
+// const modifyTodoHandler = (e) => {
+//   const modifying = "lnr lnr-undo";
+//   const doneEdit = "lnr lnr-checkmark-circle";
+//   if (!e.target.matches(".modify span")) return;
+//   if (e.target.className === modifying) {
+//     e.target.className = doneEdit;
+//     const $editLi = e.target.closest(".todo-list-item");
+//     console.log($editLi);
+//     const $editSpan = $editLi.querySelector(".checkbox span");
+//     const originText = $editSpan.textContent;
+//     const $newedit = document.createElement("input");
+//     $newedit.value = originText;
+//     $newedit.setAttribute("class", "text");
+//     $newedit.classList.add("modify-input");
+//     // $newedit.classList.add('modify-input:focus');
+//     $newedit.setAttribute("id", "edit-text");
+//     console.log($newedit);
+//     const parentDiv = $editSpan.parentNode;
+//     console.log(parentDiv);
+//     parentDiv.replaceChild($newedit, $editSpan);
+//   } else {
+//     e.target.className = modifying;
+//     const id = e.target.closest(".todo-list-item").dataset.id;
+//     const $edit=document.getElementById('edit-text');
+//     const text =$edit.value;
+//     // console.log();
+//     const payload = {
+//       text: text,
+//     };
+//     fetchTodos(`${URL}/${id}`, "PATCH", payload).then((res) => {
+//       if (res.status === 200 || res.status === 201) {
+//         alert("수정 성공!");
+//       } else {
+//         alert("수정 실패!");
+//       }
+//     });
+//   }
+// };
 
 
-//======================= 앱실행 ============================//
+
+// $todoList.addEventListener("click", modifyTodoHandler);
+$todoList.addEventListener("click", modifyTodoHandler);
+// $todoList.addEventListener('click', doneEditTodoHandler);
+// =========== 앱 실행 =========== //
 const init = () => {
   fetchTodos(URL)
     .then((res) => res.json())
